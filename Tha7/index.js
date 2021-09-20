07a5160b28f36d11538e4ac1645924e76f77d955
Prasanna Anjankar <prasannaanjankar@gmail.com>
2021-09-20 19:12:25 +0530

    tha 7,8,9

--- Tha7/index.js
+++ Tha7/index.js
@@ -1,7 +1,54 @@
 const express = require('express');
-
-constant express = require('express');
 const app = express();
 
+
+
+
+// const slash = (req, res) => {
+//     res.send("Hello");
+// };
+
+// app.get('/', slash);
+// app.post('/', slash);
+// app.put('/', slash);
+// app.delete('/', slash);
+
+// crud = ? >> post - send, get - read, put - , delete - delete
+app.get('/', (req, res) => {
+    // res.send('ab?cd');
+    // res.json({
+    //     a: 1,
+    //     b: 2,
+    //     c: 3
+    // });
+    res.sendStatus(500);
+});
+
+
+app.post('/', (req, res) => {
+    res.json({ text: req.body })
+});
+
+
+app.get('/ab?cd', function(req, res) {
+    res.send('ab?cd')
+});
+
+
+app.get('/ab*cd', function(req, res) {
+    res.send('ab*cd')
+});
+
+app.get('/ab+cd', function(req, res) {
+    res.send('ab+cd')
+});
+
+app.get('/.*fly$/', function(req, res) {
+    res.send('/.*fly$/')
+});
+
+app.get('users/:userId/books/:bookId', function(req, res) {
+    res.send(req.params)
+});
 
 app.listen(5000);
---
+++ Tha9/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+console.log(__dirname); //dirname current directory  kaaa name deta haoiiii
+
+app.use('/', (req, res) => {
+    // res.sendFile(path.join(__dirname, "public/Hello.txt"), 'test.txt') //path join is similar to string concatination
+    // res.sendFile(path.join(__dirname, "public/test.json"))
+
+    // res.download(path.join(__dirname, "public/test.json"), 'hello.json')
+
+    res.sendFile(path.join(__dirname, "public/owl.jpg"))
+    res.download(path.join(__dirname, "public/owl.jpg"), 'ullu.jpg')
+})
+
+//it listens a port
+app.listen(5000);
